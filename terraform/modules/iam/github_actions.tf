7665465c1a2132ef964413d69ab9c8b80ec8371d
Nabil <nabilislam.95@outlook.com>
2026-09-07 23:20:31 +0100

    fix: update GitHub Actions OIDC trust

--- terraform/modules/iam/github_actions.tf
+++ terraform/modules/iam/github_actions.tf
@@ -25,7 +25,7 @@ resource "aws_iam_role" "github_actions" {
           }
 
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
           }
         }
       }
