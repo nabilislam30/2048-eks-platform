9b518f74e65036e7e23c8a672924d9cdd4e61a65
Nabil <150935632+nabilislam30@users.noreply.github.com>
2026-09-09 12:02:53 +0100

    chore: tag Terraform state bucket

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -1,5 +1,12 @@
 resource "aws_s3_bucket" "terraform_state_bucket" {
   bucket = var.state_bucket_name
+
+  tags = {
+    Name      = var.state_bucket_name
+    Project   = var.project_name
+    ManagedBy = "Terraform"
+    Purpose   = "TerraformState"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state_bucket_versioning" {
